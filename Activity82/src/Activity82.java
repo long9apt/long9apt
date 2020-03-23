985a594aaaf3473786a4b458d9cdea6303eefd53
long9apt <62142928+long9apt@users.noreply.github.com>
2020-03-23 16:52:22 +0700

    update activity82

---
+++ Activity82/src/Activity82.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Activity82 {
+        public static void main(String[] args) {
+            List<String> list_Strings = new ArrayList<String>();
+            list_Strings.add("Red");
+            list_Strings.add("Green");
+            list_Strings.add("Orange");
+            list_Strings.add("White");
+            list_Strings.add("Black");
+            System.out.println(list_Strings);
+            list_Strings.add(3, "Yellow");
+            System.out.println(list_Strings);
+        }
+    }
